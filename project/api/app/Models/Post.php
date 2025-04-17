c7f3eaac3899d2db52d4ca8db47b070b504e16f0
JakobHuemer <j.huemer-fistelberger@htblaleonding.onmicrosoft.com>
2025-04-17 18:17:19 +0200

    refactor: remove factory references in model Post

--- project/api/app/Models/Post.php
+++ project/api/app/Models/Post.php
@@ -2,13 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Entity
 {
-    /** @use HasFactory<\Database\Factories\PostFactory> */
-    use HasFactory;
 
     protected $table = "posts";
 
